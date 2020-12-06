4ee5a0e43cce9ca9148341e8426ed79dda894d40
Jonathan Gomez <jongomezdev@gmail.com>
2020-12-06 16:21:59 -0500

    Finish logic in the timeBlockEl

--- script.js
+++ script.js
@@ -9,8 +9,30 @@ $("#currentDay").text(
 
 //Create a function that styles the timeblocks in real time
 //past, present & future
-let currentTime = luxon.DateTime.local({
-  hour: "2-digit",
-});
 
+function timeBlockEl() {
+  let hourEl = luxon.DateTime.local().toLocaleString({ hour: "2-digit" });
+
+  let formattedHour = parseInt(hourEl[0] + hourEl[1]);
+  if (hourEl.indexOf("PM")) {
+    formattedHour += 12;
+    console.log(formattedHour);
+  }
+  // console.log(formattedHour);
+  console.log(hourEl);
+
+  $(".time-block").each(function () {
+    let currentHour = parseInt($(this).attr("id"));
+    // console.log(currentHour);
+    if (formattedHour > currentHour) {
+      $(this).addClass("past");
+    } else if (currentHour === formattedHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+  });
+}
 //When save button is clicked, store input to local storage
+
+timeBlockEl();
